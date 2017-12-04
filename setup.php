3ff83d4c0a23974c568cddd601b7829092be5c98
Alexandre Delaunay <delaunay.alexandre@gmail.com>
2017-12-04 14:44:02 +0100

    version

--- setup.php
+++ setup.php
@@ -28,7 +28,7 @@
  @since     2009
  ---------------------------------------------------------------------- */
 
-define ('PLUGIN_UNINSTALL_VERSION', '2.3.0');
+define ('PLUGIN_UNINSTALL_VERSION', '2.3.1');
 
 /**
  * Function Init
